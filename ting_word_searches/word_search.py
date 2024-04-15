370e9afee10d708529eb81d759590c4c646db5b1
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-04-15 15:56:04 -0300

    req 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+    word_lower = word.lower()
+
+    for file_info in instance.items:
+        occurrences = []
+        file_lines = file_info['linhas_do_arquivo']
+        for index, line in enumerate(file_lines, start=1):
+            if word_lower in line.lower():
+                occurrences.append({"linha": index})
+
+        if occurrences:
+            results.append({
+                "palavra": word,
+                "arquivo": file_info['nome_do_arquivo'],
+                "ocorrencias": occurrences
+            })
+
+    return results
 
 
 def search_by_word(word, instance):
